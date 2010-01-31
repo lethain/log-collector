3d87dd2045aeee95785ee0a0094a209d3ce1565c
Will Larson <lethain@will-larsons-macbook.local>
2010-01-30 22:49:51 -0800

    Changes, bring inline with blog post

--- log_watcher.js
+++ log_watcher.js
@@ -1,15 +1,15 @@
 var sys = require("sys"),
-    http = require("http");
+    http = require("http"),
+    file = process.ARGV[3],
+    hostname = (process.ARGV[4]) ? process.ARGV[4] : "localhost",
+    port = (process.ARGV[5]) ? parseInt(process.ARGV[5]) : 8000,
+    log_server = http.createClient(port, hostname);
 
-var file = process.ARGV[3];
-var hostname = (process.ARGV[4]) ? process.ARGV[4] : "localhost";
-var port = (process.ARGV[5]) ? parseInt(process.ARGV[5]) : 8000;
-var log_server = http.createClient(port, hostname);
 sys.puts("sending logs to " + hostname + ":" + port);
 
 var send_log = function(msg) {
     var req = log_server.request("POST", "/", {"Content-Length":msg.length});
-    req.sendBody(msg, encoding="ascii")
+    req.sendBody(msg, encoding="ascii");
     req.finish();
 }
 
@@ -17,7 +17,7 @@ var monitor_file = function(filename) {
     sys.puts("monitor_file: monitoring " + filename);
     var cmd = process.createChildProcess("tail", ["-F", filename]);
     cmd.addListener("output", function(data) {
-	    sys.puts("output: sending (" + data.length + ")");
+	    sys.puts("output: sending (" + data.length + ")");	    
 	    send_log(data);
 	});
 };
