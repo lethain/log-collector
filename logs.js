2b481f589e6197fe536f1c06d13219ab43381e85
Will Larson <lethain@will-larsons-macbook.local>
2010-01-30 16:46:05 -0800

    Changes..

--- logs.js
+++ logs.js
@@ -1,12 +1,8 @@
 var sys = require("sys"),
     http = require("http");
 
-var backend = (process.ARGV[3]) ? process.ARGV[3] : "./log_file_backend.js";
+var backend = (process.ARGV[3]) ? process.ARGV[3] : "./log_file_backend";
 var record_message = require(backend).record_message;
-
-var record_message = function(request, msg) {
-    sys.puts("received: " + msg);
-};
 
 http.createServer(function (request, response) {
 	var content = "";
@@ -16,7 +12,7 @@ http.createServer(function (request, response) {
 	request.addListener("complete", function() {
 		record_message(request, content);
 		response.sendHeader(200, {"Content-Type": "text/plain"});
-		response.sendBody("stored message (" + content.length ")");
+		response.sendBody("stored message (" + content.length + ")");
 		response.finish();
 	    });
     }).listen(8000);
